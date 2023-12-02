d29833690e923047bfafbb6cac372d8ed88227af
savagejacke <savagejacke@vt.edu>
2023-12-02 17:13:01 -0500

    started api endpoint

---
+++ src/pages/api/autocomplete.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { OpenAI } from "openai";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (!req.body.email) {
+    res.status(400).json({ message: "Include and email" });
+  }
+
+  const openai = new OpenAI({
+    apiKey: process.env.API_KEY,
+  });
+
+  const params: OpenAI.Chat.ChatCompletionCreateParams = {
+    messages: [
+      {
+        role: "user",
+        content:
+          "Give me three ways to complete this sentence in an email: " +
+          req.body.email,
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  };
+
+  const chatCompletion: OpenAI.Chat.ChatCompletion =
+    await openai.chat.completions.create(params);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
-
 export default function Home() {
   return (
     <main>
-        <h1 className="text-4xl font-bold text-center pb-5">Jan and Jack's Super Cool Email Autocomplete</h1>
-        <div className="flex flex-col items-center">
-            <h2 className="text-2xl font-bold justify">Start writing email below</h2>
-            <textarea className="w-1/2 h-32 p-2 mt-4 text-xl border-2 border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-black" />
-            <button className="px-4 py-2 mt-4 text-xl font-bold text-white bg-blue-500 rounded-md hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-800 focus:ring-offset-2 focus:ring-offset-blue-200">Submit</button>
-        </div>
+      <h1 className="text-4xl font-bold text-center pb-5">
+        Jan and Jack&apos;s Super Cool Email Autocomplete
+      </h1>
+      <div className="flex flex-col items-center">
+        <h2 className="text-2xl font-bold justify">
+          Start writing email below
+        </h2>
+        <textarea className="w-1/2 h-32 p-2 mt-4 text-xl border-2 border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-black" />
+        <button className="px-4 py-2 mt-4 text-xl font-bold text-white bg-blue-500 rounded-md hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-800 focus:ring-offset-2 focus:ring-offset-blue-200">
+          Submit
+        </button>
+      </div>
     </main>
-  )
+  );
 }
